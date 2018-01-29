45aa9f7e0f291919031a523ce7e1d5f1c13247ca
Magno Biét <magno.biet@gmail.com>
2018-01-28 22:26:28 -0200

    Add ESLint rules

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+	"env": {
+		"es6": true,
+		"node": true,
+		"browser": true
+	},
+	"extends": [
+		"eslint:recommended"
+	],
+	"parserOptions": {
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		"no-console": ["error", {
+			"allow": ["log", "warn", "error", "debug"]
+		}],
+		"quotes": ["error", "single"],
+		"camelcase": ["error"],
+		"semi": ["error", "always"],
+	}
+};
