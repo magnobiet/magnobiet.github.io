676db0356aa77c7c783040a13a66b7c05f16bf9c
Magno Fernando Brixner Biét <magno.biet@cwi.com.br>
2019-12-30 16:57:42 -0300

    chore: update ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
-	"env": {
-		"es6": true,
-		"node": true,
-		"browser": true
+	env: {
+		es6: true,
+		node: false,
+		browser: true
 	},
-	"extends": [
-		"eslint:recommended"
+	extends: [
+		'eslint:recommended'
 	],
-	"parserOptions": {
-		"sourceType": "module"
+	parserOptions: {
+		sourceType: 'module'
 	},
-	"rules": {
-		"indent": ["error", "tab"],
-		"linebreak-style": ["error", "unix"],
-		"no-console": ["error", {
-			"allow": ["log", "warn", "error", "debug"]
+	rules: {
+		'indent': ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		'no-console': ['error', {
+			'allow': ['log', 'warn', 'error', 'debug']
 		}],
-		"quotes": ["error", "single"],
-		"camelcase": ["error"],
-		"semi": ["error", "always"],
+		'quotes': ['error', 'single'],
+		'camelcase': ['error'],
+		'semi': ['error', 'always'],
 	}
 };
