b354f0747ba563bfd0ca88928c6dc3cc2880c854
Magno Biét <magno.biet@gmail.com>
2019-12-30 20:17:38 -0300

    feat: add service worker

---
+++ src/sw.js
@@ -0,0 +1,39 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+if (workbox) {
+
+	workbox.setConfig({
+		debug: true
+	});
+
+	workbox.core.setCacheNameDetails({
+		prefix: 'magnobiet-com',
+		suffix: 'v1'
+	});
+
+	workbox.routing.registerRoute(
+		/^https:\/\/fonts\.googleapis\.com/,
+		new workbox.strategies.StaleWhileRevalidate({ cacheName: 'google-fonts-stylesheets' })
+	);
+
+	workbox.routing.registerRoute(
+		/\.(?:js|css|html|json)$/,
+		new workbox.strategies.StaleWhileRevalidate()
+	);
+
+	workbox.routing.registerRoute(
+		/\.(?:png|gif|jpg|svg|ico)$/,
+		new workbox.strategies.CacheFirst({
+			cacheName: 'images',
+			plugins: [
+				new workbox.expiration.Plugin({
+					maxEntries: 60,
+					maxAgeSeconds: 30 * 24 * 60 * 60
+				})
+			]
+		})
+	);
+
+	workbox.precaching.precacheAndRoute([]);
+
+}
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	globDirectory: 'dist/',
+	globPatterns: [
+		'**/*.{html,png,js,ico,css,svg,webmanifest}'
+	],
+	swDest: 'dist/sw.js',
+	swSrc: 'src/sw.js'
+};
